efc6b76c942166933ac215ccb9b9024a46678cab
Jorge Salgueiro <yorch.academico@gmail.com>
2025-10-13 22:42:54 -0400

    Agregado InteractiveDelay

--- Pi2W/L6ProyPico2W.py
+++ Pi2W/L6ProyPico2W.py
@@ -12,7 +12,7 @@ import time
 
 # Configuraciones de pines:
 #-------------------------{ Leds
-led1 = Pin(10,Pin.OUT)
+led1 = Pin(10,Pin.OUT) # Actividad Uart
 led2 = Pin(11,Pin.OUT)
 led3 = Pin(12,Pin.OUT)
 led4 = Pin(13,Pin.OUT)
@@ -24,10 +24,12 @@ Buzzer = Pin(16,Pin.OUT)
 poll = select.poll()
 poll.register(sys.stdin, select.POLLIN)
 
+tIter = 0
+
 #======================================================
 
-if __name__ == "__main__":
-
+def main():
+    BuzzerState = False
     try:
         while True:
             led1.value(0)
@@ -35,7 +37,14 @@ if __name__ == "__main__":
                 led1.value(1)
                 linea = sys.stdin.readline().strip()
                 if linea == "buzzer":
-                    Buzzer.toggle()
+                    Buzzer.value(1)
+                    BuzzerState = True
+            #-----------------------
+            if BuzzerState:
+                interactiveDelay(2.0)
+                if tIter==0:
+                    BuzzerState = False
+                    Buzzer.value(0)
             time.sleep(0.1)
 
     except Exception as e:
@@ -48,3 +57,14 @@ if __name__ == "__main__":
         led1.toggle()
         led2.toggle()
         time.sleep(0.2)
+
+def interactiveDelay(time_sec):
+    global tIter
+    TotalTimeIter = int(time_sec*10)
+    if tIter==0:
+        tIter=TotalTimeIter
+    time.sleep(0.1)
+    tIter-=1
+
+if __name__ == "__main__":
+    main()
